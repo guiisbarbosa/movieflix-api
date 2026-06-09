8058ef6f61c538d95e45cbad734ba6770b5c39db
guiisbarbosa <gui.silva93@hotmail.com>
2026-06-09 20:49:02 -0300

    criada a rota de post para novos filmes

--- src/server.ts
+++ src/server.ts
@@ -5,17 +5,38 @@ const port = 3000;
 const app = express();
 const prisma = new PrismaClient();
 
+app.use(express.json());
+
 app.get('/movies', async (req, res) => {
     const movies = await prisma.movie.findMany({
         orderBy: {
-            title: 'asc'
+            title: 'asc',
         },
         include: {
             genres: true,
-            languages: true
-        }
+            languages: true,
+        },
     });
     res.json(movies);
+});
+
+app.post('/movies', async (req, res) => {
+    const { title, genre_id, language_id, oscar_count, release_date } =
+        req.body;
+    try {
+        await prisma.movie.create({
+            data: {
+                title,
+                genre_id,
+                language_id,
+                oscar_count,
+                release_date: new Date(release_date),
+            },
+        });
+    } catch (error) {
+        return res.status(500).send({ message: 'Falha no cadastro do filme' });
+    }
+    res.status(201).send();
 });
 
 app.listen(port, () => {
