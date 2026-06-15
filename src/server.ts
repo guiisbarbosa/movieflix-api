b38cf7dddbb51fb03edf544e00edc900d55b8c8f
guiisbarbosa <gui.silva93@hotmail.com>
2026-06-15 20:35:59 -0300

    Adicionada a rota de remoção de filmes

--- src/server.ts
+++ src/server.ts
@@ -79,6 +79,26 @@ app.put('/movies/:id', async (req, res) => {
     res.status(200).send();
 });
 
+app.delete('/movies/:id', async (req, res) => {
+    const id = Number(req.params.id);
+    try {
+        const movie = await prisma.movie.findUnique({
+            where: { id },
+        });
+
+        if (!movie) {
+            return res.status(404).send({ message: 'Filme não encontrado' });
+        }
+
+        await prisma.movie.delete({
+            where: { id },
+        });
+    } catch (error) {
+        return res.status(500).send({ message: 'Falha na exclusão do filme' });
+    }
+    res.status(200).send();
+});
+
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
