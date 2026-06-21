526180d43889fe290fc81a515b550823e97c19dd
guiisbarbosa <gui.silva93@hotmail.com>
2026-06-20 23:38:03 -0300

    "adicionado swagger com cadastro de filmes

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,15 @@
 import express from 'express';
 import { PrismaClient } from '../generated/prisma/index.js';
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from "../swagger.json" with { type: "json" };
 
 const port = 3000;
 const app = express();
 const prisma = new PrismaClient();
 
 app.use(express.json());
+
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.get('/movies', async (req, res) => {
     const movies = await prisma.movie.findMany({
